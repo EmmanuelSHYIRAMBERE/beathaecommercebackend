9341655cbf2296c0cee7797060ab70dbd08bba80
Emmanuel SHYIRAMBERE <emashyirambere1@gmail.com>
2023-12-18 09:37:35 +0200

    slots update

--- controllers/Parking/checkAvilableParkingsByUser.js
+++ controllers/Parking/checkAvilableParkingsByUser.js
@@ -6,28 +6,25 @@ export const checkAvailableParkingsByUser = catchAsyncError(
   async (req, res, next) => {
     const { id } = req.params;
 
-    const floors = await Floors.find({ buildingId: id });
+    const buildingFloors = await Floors.find({ buildingId: id });
 
-    if (!floors || floors.length === 0) {
+    if (!buildingFloors || buildingFloors.length === 0) {
       return next(new errorHandler(`No floors found!`, 401));
     }
 
-    const totalFloors = floors.length;
-
-    const slots = await Promise.all(
-      floors.map(async (floor) => {
+    const floors = await Promise.all(
+      buildingFloors.map(async (floor) => {
         const floorSlots = await Parkings.find({ floorID: floor._id });
-        return { floorName: floor.Name, slots: floorSlots };
+        return { _id: floor._id, floorName: floor.Name, floorSlots };
       })
     );
 
-    if (!slots || slots.length === 0) {
+    if (!floors || floors.length === 0) {
       return next(new errorHandler(`No slots found!`, 401));
     }
 
     res.status(200).json({
-      Floors: totalFloors,
-      slots,
+      floors,
     });
   }
 );
