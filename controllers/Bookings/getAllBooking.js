de5a0004da8a5c699dc90f5e22fcd463f3014cfd
Emmanuel SHYIRAMBERE <emashyirambere1@gmail.com>
2023-12-22 09:21:43 +0200

    floors

--- controllers/Bookings/getAllBooking.js
+++ controllers/Bookings/getAllBooking.js
@@ -2,16 +2,42 @@
 //   res.status(200).json(res.paginatedResults);
 // };
 
-import { Reservations } from "../../models";
+import { Cars, Parkings, Reservations, User } from "../../models";
 import { catchAsyncError } from "../../utility";
 import errorHandler from "../../utility/errorHandlerClass";
 
 export const getBookings = catchAsyncError(async (req, res, next) => {
-  const bookings = await Reservations.find({});
+  try {
+    const bookings = await Reservations.find({});
 
-  if (!bookings) {
-    return next(new errorHandler(`Nothing found in database`, 404));
+    if (!bookings || bookings.length === 0) {
+      return next(new errorHandler(`No bookings found in the database`, 404));
+    }
+
+    const floorDetails = [];
+
+    for (const booking of bookings) {
+      const user = await User.findOne({ _id: booking.userID });
+      const parkingSlot = await Parkings.findOne({ _id: booking.slotID });
+      console.log(parkingSlot);
+      const cars = await Cars.findOne({ _id: booking.carID });
+
+      floorDetails.push({
+        _id: booking._id,
+        slotName: parkingSlot ? parkingSlot.Slot : booking.slotID,
+        userName: user ? user.fullNames : booking.userID,
+        carPlateNo: cars ? cars.platNumber : booking.carID,
+        bookedDate: booking.bookedDate,
+        startHour: booking.startHour,
+        endHour: booking.endHour,
+        Status: booking.Status,
+        dateSent: booking.dateSent,
+      });
+    }
+
+    res.status(200).json(floorDetails);
+  } catch (error) {
+    console.error("Error in getBookings:", error);
+    next(new errorHandler("Internal Server Error", 500));
   }
-
-  res.status(200).json(bookings);
 });
