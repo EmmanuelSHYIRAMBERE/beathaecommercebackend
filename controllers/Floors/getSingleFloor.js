55ae7e000fae646f945db1d47f11e6b8f1e86823
Emmanuel SHYIRAMBERE <emashyirambere1@gmail.com>
2024-01-04 15:11:37 +0200

    updets

--- controllers/Floors/getSingleFloor.js
+++ controllers/Floors/getSingleFloor.js
@@ -20,16 +20,19 @@ export const getSingleFloor = catchAsyncError(async (req, res, next) => {
 
   const floor = await Floors.findOne({ _id: id });
 
-  if (!floor || floor.length === 0) {
+  if (!floor) {
     return next(new errorHandler(`Floor with this ID: ${id} not found!`, 401));
   }
 
-  const slots = await floor.map(async (floor) => {
-    const floorSlots = await Parkings.find({ floorID: floor._id });
-    return { _id: floor._id, floorName: floor.Name, slots: floorSlots };
-  });
+  const floorSlots = await Parkings.find({ floorID: floor._id });
 
-  if (!slots || slots.length === 0) {
+  const slots = {
+    _id: floor._id,
+    floorName: floor.Name,
+    slots: floorSlots,
+  };
+
+  if (!slots || Object.keys(slots).length === 0) {
     return next(new errorHandler(`No slot found!`, 401));
   }
 
