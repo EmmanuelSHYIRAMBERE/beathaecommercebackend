18c022d7601d34d048f89c0769c09fc651d0fada
Emmanuel SHYIRAMBERE <emashyirambere1@gmail.com>
2024-01-10 09:56:00 +0200

    we

--- middleware/resetPasswordEmail.js
+++ middleware/resetPasswordEmail.js
@@ -23,94 +23,80 @@ export const resetPasswordEmail = (userEmail, userNames, otpCode) => {
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Password Reset - Smart Parking System</title>
+    <title>Welcome to Smart Parking System</title>
     <style>
       body {
-        background-color: hsl(230, 19%, 81%);
+        background-color: hsl(230, 19%, 81%); /* Light Gray */
         font-family: Arial, sans-serif;
         margin: 0;
         padding: 0;
       }
-
       .header {
-        background-color: hsl(210, 100%, 76%);
-        padding: 20px;
+        background-color: hsl(210, 100%, 76%); /* Blue */
         display: flex;
         align-items: center;
-        padding: 2px 15%;
-        padding-right: 25%;
-        justify-content: space-between;
       }
-
       .header img {
         max-width: 200px;
         border-radius: 50%;
         height: auto;
-        transition: all 0.3s ease-in-out;
+        transition: all 0.3s ease-in-out; /* Add a smooth transition effect */
       }
-
       .header h1 {
-        color: hsl(328, 100%, 59%);
+        color: hsl(328, 100%, 59%); /* Dark Gray */
         font-size: 28px;
         font-weight: 700;
         margin: 0;
-        padding-bottom: 10px;
         text-align: center;
       }
-
       .content {
         max-width: 600px;
         margin: 0 auto;
         padding: 20px;
-        background-color: hsl(210, 60%, 98%);
+        margin-top: 20px;
+        background-color: hsl(210, 60%, 98%); /* White */
         border-radius: 5px;
         box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
       }
-
       h2 {
-        color: hsl(60, 100%, 25%);
+        color: hsl(60, 100%, 25%); /* Dark Gray */
         font-size: 28px;
         margin: 0;
         padding-bottom: 10px;
         text-align: center;
       }
-
       p {
-        color: hsl(0, 0%, 0%);
+        color: hsl(0, 0%, 0%); /* Gray */
         font-size: 16px;
         margin: 0;
         text-align: left;
       }
-
-      .otp-container {
-        text-align: center;
-      }
-
       .button-container {
         text-align: center;
         margin-top: 20px;
       }
-
       .button {
         display: inline-block;
         padding: 10px 20px;
-        background-color: hsl(328, 100%, 59%);
-        color: hsl(210, 60%, 98%);
+        background-color: hsl(328, 100%, 59%); /* Blue */
+        color: hsl(210, 60%, 98%); /* White */
         font-weight: bold;
         border-radius: 5px;
         text-decoration: none;
       }
-
       .button:hover {
-        background-color: hsl(25, 100%, 50%);
+        background-color: hsl(25, 100%, 50%); /* Darker Blue */
       }
-
       .footer {
         text-align: left;
         padding: 20px 5%;
         margin-top: 20px;
         font-size: 12px;
-        color: hsl(0, 0%, 0%);
+        color: hsl(0, 0%, 0%); /* Light Gray */
+      }
+      .header a {
+        margin-left: 20px;
+        padding: 10px;
       }
     </style>
   </head>
--- middleware/sendEmail.js
+++ middleware/sendEmail.js
@@ -25,7 +25,6 @@ export const sendEmail = (userEmail, userNames) => {
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Welcome to Smart Parking System</title>
-
     <style>
       body {
         background-color: hsl(230, 19%, 81%); /* Light Gray */
@@ -33,42 +32,33 @@ export const sendEmail = (userEmail, userNames) => {
         margin: 0;
         padding: 0;
       }
-
       .header {
         background-color: hsl(210, 100%, 76%); /* Blue */
-        padding: 20px;
         display: flex;
         align-items: center;
-        padding: 2px 15%;
-        padding-right: 25%;
-        justify-content: space-between;
       }
-
       .header img {
         max-width: 200px;
         border-radius: 50%;
         height: auto;
         transition: all 0.3s ease-in-out; /* Add a smooth transition effect */
       }
-
       .header h1 {
         color: hsl(328, 100%, 59%); /* Dark Gray */
         font-size: 28px;
         font-weight: 700;
         margin: 0;
-        padding-bottom: 10px;
         text-align: center;
       }
-
       .content {
         max-width: 600px;
         margin: 0 auto;
         padding: 20px;
+        margin-top: 20px;
         background-color: hsl(210, 60%, 98%); /* White */
         border-radius: 5px;
         box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
       }
-
       h2 {
         color: hsl(60, 100%, 25%); /* Dark Gray */
         font-size: 28px;
@@ -76,19 +66,16 @@ export const sendEmail = (userEmail, userNames) => {
         padding-bottom: 10px;
         text-align: center;
       }
-
       p {
         color: hsl(0, 0%, 0%); /* Gray */
         font-size: 16px;
         margin: 0;
         text-align: left;
       }
-
       .button-container {
         text-align: center;
         margin-top: 20px;
       }
-
       .button {
         display: inline-block;
         padding: 10px 20px;
@@ -98,17 +85,19 @@ export const sendEmail = (userEmail, userNames) => {
         border-radius: 5px;
         text-decoration: none;
       }
-
       .button:hover {
         background-color: hsl(25, 100%, 50%); /* Darker Blue */
       }
-
       .footer {
         text-align: left;
         padding: 20px 5%;
         margin-top: 20px;
         font-size: 12px;
         color: hsl(0, 0%, 0%); /* Light Gray */
+      }
+      .header a {
+        margin-left: 20px;
+        padding: 10px;
       }
     </style>
   </head>
@@ -131,8 +120,10 @@ export const sendEmail = (userEmail, userNames) => {
         journey where finding a parking spot is just a click away.
         <br /><br />Reserve your parking spot today and experience the
         convenience!
-      </p><br />
-    </div><br />
+      </p>
+      <br />
+    </div>
+    <br />
     <div class="footer">
       Thank you for choosing Smart Parking System to simplify your parking
       experience. We look forward to providing you with seamless parking
@@ -140,11 +131,11 @@ export const sendEmail = (userEmail, userNames) => {
       functionalities of our platform. If you have any questions or need
       assistance, feel free to reach out. We are here to make your parking
       experience as efficient and enjoyable as possible.<br /><br />
-
       Best regards,
     </div>
   </body>
 </html>
+
 
 
 `,
