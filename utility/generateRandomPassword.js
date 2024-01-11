0022f2fb96a01e5e6f9dc443144e7677d82c1d36
Emmanuel SHYIRAMBERE <emashyirambere1@gmail.com>
2024-01-11 11:18:30 +0200

    we

--- utility/generateRandomPassword.js
+++ utility/generateRandomPassword.js
@@ -1,6 +1,6 @@
 export const generateRandomPassword = (length) => {
   const charset =
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{};:\\|,.<>/?";
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$";
 
   let password = "";
   for (let i = 0; i < length; i++) {
